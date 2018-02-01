6cab24e6890ed7c0a98833956bbd63bf54fb202c
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-02-01 22:32:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,24 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  });
+}
+
+function frameIt(){
+$('img').on('load', function(){
+  $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $('typing').on('keydown', function(){
+    if(key.which == 71){
+      alert('G key was pressed down');
+    }
+  })
+}
+
 
 $(document).ready(function(){
 
